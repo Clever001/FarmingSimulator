2747c1c59372054426b7ee299c6a079bf4b07cfc
Nikon Vitaly <cakevit@gmail.com>
2024-10-01 15:06:44 +0500

    Before swaping List<miners> with key - value container.

--- FarmingClasses/Builders/AutoMinerBuilder.cs
+++ FarmingClasses/Builders/AutoMinerBuilder.cs
@@ -1,16 +1,15 @@
 ﻿using FarmingClasses.Other;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace FarmingClasses.Builders;
 public class AutoMinerBuilder {
-    public AutoMiner GetWaterer() => new("Поливальщик", 20, 2);
+    public AutoMiner GetWaterer() => new("Поливальщик", "Поливальщики", 20, 2);
 
-    public AutoMiner GetFertilizer() => new("Удобритель", 40, 4);
+    public AutoMiner GetFertilizer() => new("Удобритель", "Удобрители", 40, 4);
 
-    public AutoMiner GetHarvester() => new("Сборщик урожая", 30, 3);
+    public AutoMiner GetHarvester() => new("Сборщик урожая", "Сборщики урожая", 30, 3);
 
-    public AutoMiner GetExpertGardener() => new("Садовод эксперт", 100, 15);
+    public AutoMiner GetExpertGardener() => new("Садовод эксперт", "Садоводы эксперты", 100, 15);
 
     public IEnumerable<IBuyable> GetAll() {
         List<IBuyable> miners = [GetWaterer(), GetFertilizer(), GetHarvester(), GetExpertGardener()];
